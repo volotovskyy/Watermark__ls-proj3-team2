e0c1c8e6169dd5df1754a4fadda90db54c1ff7a3
Bogdan Volotovskyy <punksnotdied1@gmail.com>
2016-01-08 14:04:20 +0200

    create markdonw branch

--- - dev/scripts/main.js
+++ - dev/scripts/main.js
@@ -1,16 +1,34 @@
-// Fake input
+// Fake input main img
 $(document).ready(function() {
 
 	var setUpListnerFileupload = function (){
-		$('#fileupload').on('change', changefileUpload);
+		$('#main_img').on('change', changefileUpload);
 	};
     
-    var $inputfile = $("#add_project_picture");
 	var changefileUpload = function (){
 		var 
-			inputFile = $('#fileupload'),
+			inputFile = $('#main_img'),
             filepath = inputFile.val(),	
-            input = $("#filename");
+            input = $("#main_img_name");
+            
+            filepath = filepath.replace(/c:\\fakepath\\/gmi,""),
+            input.val(filepath)
+	};
+
+	setUpListnerFileupload();
+
+	changefileUpload();
+	
+	var setUpListnerFileupload = function (){
+	$('#watermark').on('change', changefileUpload);
+	};
+    
+	
+	var changefileUpload = function (){
+		var 
+			inputFile = $('#watermark'),
+            filepath = inputFile.val(),	
+            input = $("#watermark-name");
             
             filepath = filepath.replace(/c:\\fakepath\\/gmi,""),
             input.val(filepath)
@@ -20,3 +38,18 @@ $(document).ready(function() {
 
 	changefileUpload();
 });
+
+// opacity slider
+$(function() {
+    $( ".opacity__slider" ).slider({
+      range: "min",
+      value: 37,
+      min: 1,
+      max: 700,
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "$" + ui.value );
+      }
+    });
+
+  });
+
