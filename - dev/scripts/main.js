0910732e18a5c1711bf4a3b04cfd33ea1765c9da
Leonid Verkhovtsev <leoromanovich@gmail.com>
2016-01-10 15:10:32 +1000

    end of common part

--- - dev/scripts/main.js
+++ - dev/scripts/main.js
@@ -1,16 +1,34 @@
-// Fake input
+// Fake input main img
 $(document).ready(function() {
 
 	var setUpListnerFileupload = function (){
-		$('#fileupload').on('change', changefileUpload);
+		$('#main_img').on('change', changefileUpload);
 	};
     
-    var $inputfile = $("#add_project_picture");
 	var changefileUpload = function (){
 		var 
-			inputFile = $('#fileupload'),
+			inputFile = $('#main_img'),
             filepath = inputFile.val(),	
-            input = $("#filename");
+            input = $("#main_img_name");
+            
+            filepath = filepath.replace(/c:\\fakepath\\/gmi,""),
+            input.val(filepath)
+	};
+
+	setUpListnerFileupload();
+
+	changefileUpload();
+	
+	var setUpListnerFileupload = function (){
+	$('#watermark').on('change', changefileUpload);
+	};
+    
+	
+	var changefileUpload = function (){
+		var 
+			inputFile = $('#watermark'),
+            filepath = inputFile.val(),	
+            input = $("#watermark-name");
             
             filepath = filepath.replace(/c:\\fakepath\\/gmi,""),
             input.val(filepath)
@@ -20,3 +38,20 @@ $(document).ready(function() {
 
 	changefileUpload();
 });
+
+// opacity slider
+$(function() {
+    $( ".opacity__slider" ).slider({
+      range: "min",
+      value: 37,
+      min: 0,
+      max: 1,
+	  step: 0.1,
+      slide: function( event, ui ) {
+        $( "#opacity__input" ).val( "$" + ui.value );
+      }
+    });
+	$( "#opacity__input" ).val( "$" + $( ".opacity__slider" ).slider( "value" ) );
+  });
+  });
+
