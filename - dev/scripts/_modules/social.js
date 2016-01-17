9a83ecef0b14b88f525678d21aab0029606e6379
YustinRD <YustinRD@gmail.com>
2016-01-17 23:03:45 +0300

    social fix#1

--- - dev/scripts/_modules/social.js
+++ - dev/scripts/_modules/social.js
@@ -1,34 +1,30 @@
 var Social = (function () {
     var share = {
-            vk: function (purl, ptitle, pimg, text) {
-                url = 'http://vkontakte.ru/share.php?';
-                url += 'url=' + encodeURIComponent(purl);
-                url += '&title='       + encodeURIComponent(ptitle);
-                url += '&description=' + encodeURIComponent(text);
-                url += '&image='       + encodeURIComponent(pimg);
-                url += '&noparse=true';
-                popup(url);
-            },
-            fb: function (purl, ptitle, pimg, text) {
-                url = 'http://www.facebook.com/sharer.php?s=100';
-                url += '&p[title]=' + encodeURIComponent(ptitle);
-                url += '&p[summary]=' + encodeURIComponent(text);
-                url += '&p[url]=' + encodeURIComponent(purl);
-                url += '&p[images][0]=' + encodeURIComponent(pimg);
-                popup(url);
-            },
-            tw: function (purl, ptitle) {
-                url = 'http://twitter.com/share?';
-                url += 'text=' + encodeURIComponent(ptitle);
-                url += '&url=' + encodeURIComponent(purl);
-                url += '&counturl=' + encodeURIComponent(purl);
-                popup(url);
-            }
+        vk: function (purl, ptitle, pimg, text) {
+            url = 'http://vkontakte.ru/share.php?';
+            url += 'url=' + encodeURIComponent(purl);
+            url += '&title=' + encodeURIComponent(ptitle);
+            url += '&description=' + encodeURIComponent(text);
+            url += '&image=' + encodeURIComponent(pimg);
+            url += '&noparse=true';
+            popup(url);
         },
-        url = 'http://bogdanbk.bget.ru/',
-        title = 'Watermarks Generator',
-        text = title,
-        img  = 'http://bogdanbk.bget.ru/img/proj.jpg';
+        fb: function (purl, ptitle, pimg, text) {
+            url = 'http://www.facebook.com/sharer.php?s=100';
+            url += '&p[title]=' + encodeURIComponent(ptitle);
+            url += '&p[summary]=' + encodeURIComponent(text);
+            url += '&p[url]=' + encodeURIComponent(purl);
+            url += '&p[images][0]=' + encodeURIComponent(pimg);
+            popup(url);
+        },
+        tw: function (purl, ptitle) {
+            url = 'http://twitter.com/share?';
+            url += 'text=' + encodeURIComponent(ptitle);
+            url += '&url=' + encodeURIComponent(purl);
+            url += '&counturl=' + encodeURIComponent(purl);
+            popup(url);
+        }
+    };
 
 //onclick="Share.vkontakte('https://vk.com/loftschoolweb112015','TITLE','IMG_PATH','DESC')"
     function popup(url) {
@@ -37,8 +33,12 @@ var Social = (function () {
 
     function _eventListener() {
         $('.share-btn').on('click', function () {
-            var social = $(this).attr('social');
-            if(social)share[social](url,title,img,text)
+            var social = $(this).attr('social'),
+                url = 'http://bogdanbk.bget.ru/',
+                title = 'Watermarks Generator',
+                text = title,
+                img = 'http://bogdanbk.bget.ru/img/proj.jpg';
+            if (social)share[social](url, title, img, text)
         });
     }
 
